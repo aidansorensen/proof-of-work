2f0ca67de4fae7dade10729ac95f0a44cb16cc87
aidansorensen <aidanswims00@gmail.com>
2022-11-28 19:20:20 -0500

    finished proof-of-work project 4

--- pow_m12865327/src/main.py
+++ pow_m12865327/src/main.py
@@ -1,17 +1,26 @@
-import numpy
-import Crypto
+# import numpy
+# import Crypto
 import os
-import hashlib
+import time
+from hashlib import sha256
+
 
 os.chdir("../")
 path = os.getcwd()
 
 
-def target_gen():
-    # yuh
-    # take user input for target generation. Then do stuff
-    difficulty = input("Input difficulty for target generation(integer): ")
-    difficulty = int(difficulty)
+def target_gen(difficulty=None):
+
+    print("****Target Generation function**** \n\n")
+
+    # for use in my time experiments. If there is an input difficulty, skip the user input bullshit
+    if difficulty is None:
+        difficulty = input("Input difficulty for target generation(integer): ")
+        difficulty = int(difficulty)
+
+
+
+    # creates target to display. The real target is a large integer, so I will save it rather than show it.
     target = ''
     for i in range(256):
         if i < difficulty:
@@ -20,39 +29,71 @@ def target_gen():
             target += '1'
 
     print(f"generated target: {target}")
-    print(f"Target length: {len(target)}")
 
+    real_target = 2 ** (256 - difficulty)
+    print(f"Bit target: {real_target}")
     # Write generated target to txt file in data/
     with open(f'{os.getcwd()}/data/target.txt', 'w') as f:
-        f.write(target)
+        f.write(str(real_target))
 
 
 def solution_gen():
     """
-    find solution to target given the message as well
-    :return:
+    find solution to target given the message
+    :param d: difficulty of solution necessary
+    :return: solution. Will be an integer number that, combined with the message, has trailing zeroes of equal value to
+    the target.
     """
+
+    print("Solution generation function. Generates nonces until a solution satsifies the target.\n\n")
+
+    max_nonce = 2 ** 32
+    # Open the target file
     with open(f'{os.getcwd()}/data/target.txt', 'r') as f:
         target = f.read()
 
-
+    # Open the message file, i.e. the Bearcat ID (mine will be m12865327)
     with open(f'{os.getcwd()}/data/input.txt', 'r') as f:
         message = f.read()
 
-    # so, with target and message, make some kind of for loop that tries to find a hash between them
-    # that has target's number of leading zeros? i'm so confused...
+    target = int(target)
+    # in this case, nonce is a potential solution. Nonce is simply iterating and getting larger as the loop goes
+    for nonce in range(max_nonce):
+        hash_result = sha256(bytes(str(message), 'utf-8')+bytes(str(nonce), 'utf-8')).hexdigest()
+        # check if this is a valid result, below the target
+        if int(hash_result, base=16) < target:
+            print(f"Nonce that provided solution: {nonce}")
+            print(f"Hash is {hash_result}")
+            break
 
-    # take message and concatenate it with SOLUTION, and if the hash of this has the same number of leading zeros as
-    # the target, then return 1. If that's wrong, idk. It sounds wrong
-
-
-
-    m = hashlib.sha256()
-    m.update(bytes(message))
+    with open(f'{os.getcwd()}/data/solution.txt', 'w') as f:
+        f.write(str(nonce))
 
 
 def verification():
-    pass
+    """
+    verify the solution from solution_gen(). If solution is valid, print 1. If the solution does not satisfy the
+    parameters, print 0
+    :param d:difficulty
+    :return: null
+    """
+    print("Verification function: Check if the solution satisfies the target\n\n")
+    with open(f'{os.getcwd()}/data/target.txt', 'r') as f:
+        target = f.read()
+    with open(f'{os.getcwd()}/data/input.txt', 'r') as f:
+        message = f.read()
+    with open(f'{os.getcwd()}/data/solution.txt', 'r') as f:
+        sol = f.read()
+
+    target = int(target)
+
+    # check if solution is valid, meaning hash(message||solution) < target
+    if int(sol, base=16) < target:
+        print("1")
+        print("Solution meets target's requirements.")
+    else:
+        print("0")
+        print("Solution does not meet target's requrements.")
 
 
 def main():
@@ -60,8 +101,19 @@ def main():
     Here's the main function. I will call the functions named after the assignment sections here in order.
     :return: null
     """
-    target_gen()
-    solution_gen()
+
+    # target_gen()
+    # solution_gen()
+    # verification()
+
+    for i in range(16,25):
+        target_gen(i)
+        start = time.time()
+        solution_gen()
+        end = time.time()
+        verification()
+        total_time = end - start
+        print(f"Elapsed time to find solution: {total_time} seconds")
 
 
 if __name__ == "__main__":
